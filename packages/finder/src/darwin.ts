1c18bfc24513f67e7244dd8d442113e958dd0530
Wayne <25836646+srk24@users.noreply.github.com>
2023-02-25 12:13:46 +0800

    add microsoft edge

--- packages/finder/src/darwin.ts
+++ packages/finder/src/darwin.ts
@@ -4,8 +4,12 @@ import { canAccess, newLineRegex, sort } from './utils'
 
 const suffixes = [
   '/Contents/MacOS/Google Chrome Canary',
+  '/Contents/MacOS/Google Chrome Dev',
   '/Contents/MacOS/Google Chrome',
   '/Contents/MacOS/Chromium',
+  '/Contents/MacOS/Microsoft Edge Canary',
+  '/Contents/MacOS/Microsoft Edge Dev',
+  '/Contents/MacOS/Microsoft Edge',
 ]
 
 const LSREGISTER =
@@ -18,7 +22,7 @@ export default function darwin() {
 
   execSync([
     `${LSREGISTER} -dump`,
-    `grep -E -i -o '/.+(google chrome( canary)?|chromium)\\.app(\\s|$)'`,
+    `grep -E -i -o '/.+(((google chrome)|(microsoft edge))( (dev|canary))?|chromium)\\.app(\\s|$)'`,
     `grep -E -v 'Caches|TimeMachine|Temporary|/Volumes|\\.Trash'`,
   ].join(' | '))
     .toString()
@@ -34,15 +38,27 @@ export default function darwin() {
 
   // Retains one per line to maintain readability.
   const priorities = [
+    { regex: new RegExp(`^${process.env.HOME}/Applications/.*Microsoft Edge.app`), weight: 46 },
+    { regex: new RegExp(`^${process.env.HOME}/Applications/.*Microsoft Edge Dev.app`), weight: 47 },
+    { regex: new RegExp(`^${process.env.HOME}/Applications/.*Microsoft Edge Canary.app`), weight: 48 },
     { regex: new RegExp(`^${process.env.HOME}/Applications/.*Chromium.app`), weight: 49 },
     { regex: new RegExp(`^${process.env.HOME}/Applications/.*Chrome.app`), weight: 50 },
-    { regex: new RegExp(`^${process.env.HOME}/Applications/.*Chrome Canary.app`), weight: 51 },
+    { regex: new RegExp(`^${process.env.HOME}/Applications/.*Chrome Dev.app`), weight: 51 },
+    { regex: new RegExp(`^${process.env.HOME}/Applications/.*Chrome Canary.app`), weight: 52 },
+    { regex: /^\/Applications\/.*Microsoft Edge.app/, weight: 96 },
+    { regex: /^\/Applications\/.*Microsoft Edge Dev.app/, weight: 97 },
+    { regex: /^\/Applications\/.*Microsoft Edge Canary.app/, weight: 98 },
     { regex: /^\/Applications\/.*Chromium.app/, weight: 99 },
     { regex: /^\/Applications\/.*Chrome.app/, weight: 100 },
-    { regex: /^\/Applications\/.*Chrome Canary.app/, weight: 101 },
+    { regex: /^\/Applications\/.*Chrome Dev.app/, weight: 101 },
+    { regex: /^\/Applications\/.*Chrome Canary.app/, weight: 102 },
+    { regex: /^\/Volumes\/.*Microsoft Edge.app/, weight: -6 },
+    { regex: /^\/Volumes\/.*Microsoft Edge Dev.app/, weight: -5 },
+    { regex: /^\/Volumes\/.*Microsoft Edge Canary.app/, weight: -4 },
     { regex: /^\/Volumes\/.*Chromium.app/, weight: -3 },
     { regex: /^\/Volumes\/.*Chrome.app/, weight: -2 },
-    { regex: /^\/Volumes\/.*Chrome Canary.app/, weight: -1 }
+    { regex: /^\/Volumes\/.*Chrome Dev.app/, weight: -1 },
+    { regex: /^\/Volumes\/.*Chrome Canary.app/, weight: 0 },
   ]
 
   return sort(installations, priorities)
