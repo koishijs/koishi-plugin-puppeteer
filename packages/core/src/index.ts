cbfad8fb2da09c9cf7c6aa84948cf7fa43dfdbb2
student_2333 <lgc2333@126.com>
2024-05-19 17:59:41 +0800

    refactor

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -144,8 +144,7 @@ class Puppeteer extends Service {
 namespace Puppeteer {
   export const filter = false
 
-  type NotUndefined<T> = T extends undefined ? never : T
-  type LaunchOptions = NotUndefined<Parameters<typeof puppeteer.launch>[0]>
+  type LaunchOptions = Exclude<Parameters<typeof puppeteer.launch>[0], undefined>
 
   export interface Config extends LaunchOptions {}
 
